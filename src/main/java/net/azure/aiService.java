b507670b3788d179320428dce3739bc4a02cee49
Keith-Hughes <mrkphughes@gmail.com>
2024-01-17 19:06:51 +0200

    basic server config

--- src/main/java/net/azure/aiService.java
+++ src/main/java/net/azure/aiService.java
@@ -1,5 +1,31 @@
 package net.azure;
 
-public class aiService {
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
 
+public class aiService {
+    Javalin app;
+    int defaultPort = 7000;
+
+    public aiService(){
+        createServer();
+        createEndpoints();
+    }
+
+    private void start(){
+        app.start(defaultPort);
+    }
+    private void createServer(){
+        app = Javalin.create();
+    }
+
+    private void createEndpoints(){
+        app.get("/hello", context -> {context.result("Hello");});
+    }
+
+
+    public static void main(String[] args) {
+        aiService service = new aiService();
+        service.start();
+    }
 }
