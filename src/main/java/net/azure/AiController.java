ecfb9a1a21fa76aa29eea8f10f2c168a29a1db2a
Keith-Hughes <mrkphughes@gmail.com>
2024-01-29 12:06:26 +0200

    corrected syntax error

--- src/main/java/net/azure/AiController.java
+++ src/main/java/net/azure/AiController.java
@@ -21,12 +21,12 @@ public class AiController {
         Map<String, Object> jsonResponse = new HashMap<>();
         jsonResponse.put("response", responseText);
         context.json(jsonResponse);
-    }
+    };
 
     public static final Handler thread = context -> {
         AiThreadDO conversationThread =assistant.createThread();
         Map<String, Object> jsonResponse = new HashMap<>();
         jsonResponse.put("threadId", conversationThread.id());
         context.json(jsonResponse);
-    }
+    };
 }
