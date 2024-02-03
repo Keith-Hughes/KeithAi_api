cecc8c7822cb7c8e4bd91df87dda422389ea469e
Keith Hughes <87945276+Keith-Hughes@users.noreply.github.com>
2024-02-03 09:11:37 +0200

    Update EndPoints.java

--- src/main/java/net/azure/EndPoints.java
+++ src/main/java/net/azure/EndPoints.java
@@ -5,8 +5,6 @@ public class EndPoints {
 
         public static final String ASK_QUESTION = "/ai/{question}/{threadID}";
         public static final String GET_THREAD = "/ai/thread";
-        public static final String SCHEDULE_ACTION = "/schedule.action";
-        public static final String FETCH_TOWNS = "/fetchTowns";
 
         public static void configure(AiService server) {
             server.routes(() -> {
